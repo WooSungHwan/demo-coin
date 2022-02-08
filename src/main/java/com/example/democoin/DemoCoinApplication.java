b2acce655372694d099accad8c3d0aaab0ca2830
흑구 <doqndnffo@gmail.com>
2022-02-08 22:31:15 +0900

    Initial commit

---
+++ src/main/java/com/example/democoin/DemoCoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.democoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCoinApplication.class, args);
+    }
+
+}
