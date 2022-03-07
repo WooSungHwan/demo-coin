8237f181d2fa36d315ce63d7491c567d42b23a1c
흑구 <doqndnffo@gmail.com>
2022-03-07 23:44:14 +0900

    무한매수 되는 버그 수정.

--- src/main/java/com/example/democoin/backtest/BackTest.java
+++ src/main/java/com/example/democoin/backtest/BackTest.java
@@ -12,13 +12,9 @@ import com.example.democoin.upbit.db.repository.FiveMinutesCandleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
-import java.sql.Timestamp;
 import java.text.DecimalFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.TemporalField;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -31,14 +27,17 @@ import static com.example.democoin.upbit.enums.OrdSideType.BID;
 @Component
 public class BackTest {
 
+    // -26.48654437695176
+
     private final FiveMinutesCandleRepository fiveMinutesCandleRepository;
     private final AccountCoinWalletRepository accountCoinWalletRepository;
     private final BackTestOrdersRepository backTestOrdersRepository;
 
-    double 시작잔고 = 100000.0;
+    double balance = 100000.0;
     double bidPrice = 10000.0; // 매수 고정 금액
     final double askPrice = 6000.0; // 매도 고정 금액
-
+    double bidFee = bidPrice * 0.0005;
+    double askFee = askPrice * 0.0005;
     String targetMarket = "KRW-BTC";
 
     DecimalFormat df = new DecimalFormat("###,###"); // 출력 숫자 포맷
@@ -53,16 +52,13 @@ public class BackTest {
             int limit = 200;
             int offset = (page - 1) * limit;
             List<FiveMinutesCandle> fiveMinutesCandles = fiveMinutesCandleRepository.findFiveMinutesCandlesLimitOffset(
-                    LocalDateTime.of(2017, 11, 1, 0, 0, 0), limit, offset);
+                    LocalDateTime.of(2018, 1, 1, 0, 0, 0), limit, offset);
 
             for (int i = 0; i < fiveMinutesCandles.size(); i++) {
                 if (i < 3) {
                     continue;
                 }
                 FiveMinutesCandle baseCandle = fiveMinutesCandles.get(i);
-                if (Objects.isNull(baseCandle)) {
-                    System.out.println();
-                }
 //                log.info("KST {} 캔들 백테스팅 시작", target.getCandleDateTimeKst());
                 if (baseCandle.getTimestamp() == null) {
                     continue;
@@ -84,7 +80,6 @@ public class BackTest {
                 Double openingPrice = targetCandle.getOpeningPrice(); // 다음캔들 시가
                 final double bidVolume = bidPrice / openingPrice; // 시가로 매수할 거래량 계산
                 final double askVolume = askPrice / openingPrice; // 시가로 매도할 거래량 계산
-                final double fee = bidPrice * 0.0005; // 수수료 계산
 
                 AccountCoinWallet wallet = accountCoinWalletRepository.findByMarket(targetMarket);
                 boolean isAskable = 매도가능(wallet);
@@ -102,7 +97,7 @@ public class BackTest {
                 if (isAsk) {
                     log.info("{} 현재 캔들", targetCandle.getCandleDateTimeKst());
                     printWalletBalance("매도 전", targetMarket);
-                    매도(targetCandle, openingPrice, askVolume, fee, wallet);
+                    매도(targetCandle, openingPrice, askVolume, askFee, wallet);
                     printWalletBalance("매도 후", targetMarket);
                     log.info("");
                 }
@@ -112,12 +107,12 @@ public class BackTest {
                 if (isBid) {
                     log.info("{} 현재 캔들", targetCandle.getCandleDateTimeKst());
                     printWalletBalance("매수 전", targetMarket);
-                    매수(targetCandle, openingPrice, bidPrice, fee, bidVolume);
+                    매수(targetCandle, openingPrice, bidPrice, bidFee, bidVolume);
                     printWalletBalance("매수 후", targetMarket);
                     log.info("");
                 }
 
-                if (LocalDateTime.of(2018, 1, 1, 0, 0, 0).isBefore(targetCandle.getCandleDateTimeKst())) {
+                if (LocalDateTime.of(2018, 2, 1, 0, 0, 0).isBefore(targetCandle.getCandleDateTimeKst())) {
                     return;
                 }
             }
@@ -128,13 +123,14 @@ public class BackTest {
     private void printWalletBalance(String beforeAfterTrade, String market) {
         AccountCoinWallet wallet = accountCoinWalletRepository.findByMarket(market);
         if (Objects.nonNull(wallet)) {
-            log.info("[{}] {} 평단가 : {}, 수익률 : {}%, 보유코인 : {}, KRW : {}원"
+            log.info("[{}] {} 평단가 : {}, 수익률 : {}%, 수익금 : {}, KRW : {}원, 잔고 : {}"
                     , beforeAfterTrade
                     , wallet.getMarket() // 코인 시장
                     , df.format(wallet.getAvgPrice()) // 평단가
                     , wallet.수익률() // 수익률
-                    , wallet.getVolume() // 보유코인
-                    , df.format(wallet.getAllPrice())); // 총 매수가
+                    , df.format(wallet.getProceeds())
+                    , df.format(wallet.getAllPrice()) // 총 매수가
+                    , df.format(balance)); // 잔고
         }
     }
 
@@ -144,6 +140,8 @@ public class BackTest {
 
         // 지갑에 매수 반영
         setBidWallet(nextCandle, openingPrice, bidPrice, fee, bidVolume);
+
+        balance -= (openingPrice * bidVolume) + fee;
 
         log.info("{} 매수 발생 !! ---- 매수가 {}원 / 매수 볼륨 {}", nextCandle.getMarket(), df.format(openingPrice), bidVolume);
     }
@@ -155,14 +153,16 @@ public class BackTest {
         wallet.addAsk(openingPrice, askVolume, askPrice, fee);
         accountCoinWalletRepository.save(wallet);
 
+        balance += (openingPrice * askVolume) - fee;
+
         log.info("{} 매도 발생 !! ---- 매도가 {}원 / 매도 볼륨 {}", wallet.getMarket(), df.format(openingPrice), askVolume);
     }
 
     private boolean 매수가능() {
-        if (시작잔고 > bidPrice) {
+        if (balance > (bidPrice + bidFee)) {
             return true;
         } else {
-            log.info("매수 잔고 부족 - 현재 잔고 : {}원", df.format(시작잔고));
+            log.info("매수 잔고 부족 - 현재 잔고 : {}원", df.format(balance));
             return false;
         }
     }
