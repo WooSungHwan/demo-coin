906c7a67fb9683090c1e006338232cfd573ae83e
흑구 <doqndnffo@gmail.com>
2022-03-22 17:54:04 +0900

    매수 매도 사유 enum 추가.

---
+++ src/main/java/com/example/democoin/backtest/strategy/ask/AskReason.java
@@ -0,0 +1,53 @@
+package com.example.democoin.backtest.strategy.ask;
+
+import com.example.democoin.configuration.enums.Reason;
+import com.example.democoin.configuration.enums.EnumInterface;
+import com.example.democoin.configuration.enums.EnumInterfaceConverter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public enum AskReason implements Reason {
+
+    NO_ASK("NO_ASK", "매도안함"),
+    MAX_PROCEED_RATE_FALL("MAX_PROCEED_RATE_FALL", "최대 수익률 대비 하락 매도"),
+    STOP_LOSS("STOP_LOSS", "손절매"),
+    RSI_OVER("RSI_OVER", "RSI 지수 이상"),
+    RSI_OVER_AND_BB_UDD_TOUCH("RSI_OVER_AND_BB_UDD_TOUCH", "RSI 지수 이상, 볼린저 밴드 상단 터치"),
+    BB_WIDTH_MAX("BB_WIDTH_MAX", "볼린저 밴드 최대 확대"),
+    LDD_CHANGE_UP("LDD_CHANGE_UP", "볼린저 밴드 하단선 확장 감소"),
+    SMA5_SMA10_DEAD_CROSS("SMA5_SMA10_DEAD_CROSS", "5 이평선과 10 이평선의 데드크로스"),
+    BB_UDD_UNDERING("BB_UDD_UNDERING", "볼린저 밴드 하단선 하향돌파"),
+    RSI_UNDERING("RSI_UNDERING", "RSI 지수 하향돌파"),
+    BB_UDD_TOUCH("BB_UDD_TOUCH", "볼린저 밴드 상단 터치")
+    ;
+
+    private String type;
+    private String name;
+
+    public static final List<AskReason> NO_ASK_GROUP = List.of(NO_ASK);
+
+    public boolean isAsk() {
+        return !NO_ASK_GROUP.contains(this);
+    }
+
+    public static AskReason find(String type) {
+        return EnumInterface.find(type, values());
+    }
+
+    @JsonCreator
+    public static AskReason findToNull(String type) {
+        return EnumInterface.findToNull(type, values());
+    }
+
+    @javax.persistence.Converter(autoApply = true)
+    public static class Converter extends EnumInterfaceConverter<AskReason> {
+        public Converter() {
+            super(AskReason.class);
+        }
+    }
+}
---
+++ src/main/java/com/example/democoin/backtest/strategy/bid/BidReason.java
@@ -0,0 +1,57 @@
+package com.example.democoin.backtest.strategy.bid;
+
+import com.example.democoin.configuration.enums.Reason;
+import com.example.democoin.configuration.enums.EnumInterface;
+import com.example.democoin.configuration.enums.EnumInterfaceConverter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public enum BidReason implements Reason {
+
+    NO_BID("NO_BID", "매수안함"),
+    RSI_UNDER_AND_BB_LDD_TOUCH("RSI_UNDER_AND_BB_LDD_TOUCH", "RSI 지수 이하, 볼린저 밴드 하단선 터치"),
+    BB_REDUCE_FIVE_CANDLES("BB_REDUCE_FIVE_CANDLES", "볼린저 밴드 5개봉 연속 수축"),
+    BB_REDUCE_SEVEN_CANDLES("BB_REDUCE_FIVE_CANDLES", "볼린저 밴드 7개봉 연속 수축"),
+    THREE_NEGATIVE_CANDLE_APPEAR("THREE_NEGATIVE_CANDLE_APPEAR", "음봉 3개 연속 출현"),
+    BB_REDUCE_FIVE_CANDLES_AND_OVER_SMA20("BB_REDUCE_FIVE_CANDLES_AND_OVER_SMA20", "볼린저 밴드 5개봉 연속, 20이평 이상"),
+    BB_REDUCE_SEVEN_CANDLES_AND_OVER_SMA20("BB_REDUCE_SEVEN_CANDLES_AND_OVER_SMA20", "볼린저 밴드 7개봉 연속, 20이평 이상"),
+    BB_REDUCE_SEVEN_CANDLES_AND_OVER_SMA200("BB_REDUCE_SEVEN_CANDLES_AND_OVER_SMA200", "볼린저 밴드 7개봉 연속, 200이평 이상"),
+    RSI_UNDER_AND_RECENT_NEGATIVE_CANDLE_COUNT_OVER_THREE("RSI_UNDER_AND_RECENT_NEGATIVE_CANDLE_COUNT_OVER_THREE", "RSI 지수 이하, 최근 음봉 캔들 3개 이상"),
+    SMA5_SMA10_GOLDEN_CROSS("SMA5_SMA10_GOLDEN_CROSS", "5 이평선과 10 이평선의 골든크로스"),
+    RSI_UNDER("RSI_UNDER", "RSI 지수 이하"),
+    SMA200_OVERING_AND_BB_LDD_OVERING("SMA200_OVERING_AND_BB_LDD_OVERING", "200 이평선 상향돌파, 볼린저 밴드 하단선 상향돌파"),
+    RSI_OVERING("RSI_OVERING", "RSI 지수 상향돌파"),
+    THREE_NEGATIVE_CANDLE_APPEAR_AND_RSI_UNDER("THREE_NEGATIVE_CANDLE_APPEAR_AND_RSI_UNDER", "음봉 연속 3회 출현, RSI 지수 이하"),
+    THREE_NEGATIVE_CANDLE_APPEAR_AND_RSI_UNDER_AND_BB_LDD_TOUCH("THREE_NEGATIVE_CANDLE_APPEAR_AND_RSI_UNDER_AND_BB_LDD_TOUCH", "음봉 3개 연속출현, RSI 지수 이하, 볼린저 밴드 하단 이하")
+    ;
+
+    private String type;
+    private String name;
+
+    public static final List<BidReason> NO_BID_GROUP = List.of(NO_BID);
+
+    public boolean isBid() {
+        return !NO_BID_GROUP.contains(this);
+    }
+
+    public static BidReason find(String type) {
+        return EnumInterface.find(type, values());
+    }
+
+    @JsonCreator
+    public static BidReason findToNull(String type) {
+        return EnumInterface.findToNull(type, values());
+    }
+
+    @javax.persistence.Converter(autoApply = true)
+    public static class Converter extends EnumInterfaceConverter<BidReason> {
+        public Converter() {
+            super(BidReason.class);
+        }
+    }
+}
